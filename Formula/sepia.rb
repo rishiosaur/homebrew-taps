a375eb1473d934a22818df221113c28af0bf6382
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-25 13:17:41 -0500

    Brew formula update for sepia version v0.2.0

--- Formula/sepia.rb
+++ Formula/sepia.rb
@@ -2,24 +2,24 @@
 class Sepia < Formula
   desc "A minimal interpreted language."
   homepage "https://github.com/rishiosaur/sepia"
-  version "0.1.4"
+  version "0.2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/rishiosaur/sepia/releases/download/v0.1.4/sepia_0.1.4_darwin_amd64.tar.gz"
-    sha256 "ef570bf69ae2c08e25195a6e2760aa3680e8e17a60ba4377196bdc95d540952d"
+    url "https://github.com/rishiosaur/sepia/releases/download/v0.2.0/sepia_0.2.0_darwin_amd64.tar.gz"
+    sha256 "4a3b5bd9f0e2a16276582553e5a96bcbfab3258887f52ebb8292271a713c445a"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/rishiosaur/sepia/releases/download/v0.1.4/sepia_0.1.4_linux_amd64.tar.gz"
-    sha256 "ee65c0202a64e000b90fcb3ce21ea489231bf56760a26e02aea1de438d91e150"
+    url "https://github.com/rishiosaur/sepia/releases/download/v0.2.0/sepia_0.2.0_linux_amd64.tar.gz"
+    sha256 "bd06cca5d0cd69311945bcc01413fb15454f7f306101cc9cb93e83b330687008"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/rishiosaur/sepia/releases/download/v0.1.4/sepia_0.1.4_linux_armv6.tar.gz"
-    sha256 "ee351abb5907a833ed901a36be98ca3aa7a189fb0702b7b83b3a0a6a5c5ba951"
+    url "https://github.com/rishiosaur/sepia/releases/download/v0.2.0/sepia_0.2.0_linux_armv6.tar.gz"
+    sha256 "65889fca56b1a50b1414fdd344d4e3e199f877333d1f0427827ff1f42c7e56a9"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/rishiosaur/sepia/releases/download/v0.1.4/sepia_0.1.4_linux_arm64.tar.gz"
-    sha256 "431a725466b296cc12d68ea828d2e07a0c2d0c0904ea0fee4fb74936c7eebc1a"
+    url "https://github.com/rishiosaur/sepia/releases/download/v0.2.0/sepia_0.2.0_linux_arm64.tar.gz"
+    sha256 "9f498ab7632f9a05b134a4e81101486b9b4a94f57849416e0837b965007995ee"
   end
 
   def install
